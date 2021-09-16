369aabbad1e2d49bab23b4c26f80259e8dd0cfd4
ChangyongKim0 <changyong.kim.0@gmail.com>
2021-09-16 22:20:25 +0900

    update etc.

---
+++ get_api_sample.py
@@ -0,0 +1,40 @@
+import api_requester
+
+# 요청종류 세팅
+authkey_path = 'security/authkey.json'
+base_url_path = 'security/url.json'
+authkey_type = 'WFS'
+
+# 인풋, 요청 키 값 세팅(변수 이름이나 내부 내용은 자유롭게 변경해도 됩니다.)
+input_dict = {
+    # 'bbox': '217365,447511,217636,447701,EPSG:5174',
+    'pnu': '1114010500100',
+    'maxFeatures': '100',
+    'resultType': 'results',
+    'srsName': 'EPSG:4326'
+}
+input_dict2 = {
+    'maxFeatures': '10',
+    'resultType': 'results',
+    'srsName': 'EPSG:5174'
+}
+pnu_list = ['1111017400105950132', '4145011800100320003'] # 리스트 인풋이 필요한 경우
+keys_request = ["NSDI:PNU",
+                "gml:posList",
+                "NSDI:LNM_LNDCGR_SMBOL"]
+
+# ApiRequester 생성 - 수정 X
+get_api = api_requester.ApiRequester(authkey_path, authkey_type, base_url_path)
+
+# 인풋 넣기 (여러번 넣어도 됨.)
+# get_api.setInput(input_dict) # 단순 인풋
+get_api.setInput(input_dict) # 단순 인풋
+# get_api.setInput(input_dict)
+# get_api.setInputs(input_dict, 'pnu', pnu_list) # 특정 키에 대해 리스트 입력이 필요한 경우, input dict 안에 해당 키가 있으면 덮어씌움.
+
+# 아웃풋 파일 이름 설정
+get_api.setOutputName('./output', 'juffddfdfnggu') 
+
+# ApiRequester 작동
+# get_api.getOutputs() # 요청 키가 없는 경우 로우 파일이 생성됨.
+get_api.getOutputs(keys_request) # 요청 키가 있는 경우 해당 키에 대한 정보만 받아옴.

ea7cb9f10aa4c701832bf5810ab284946c867a28
ChangyongKim0 <changyong.kim.0@gmail.com>
2021-10-08 09:51:05 +0900

    missing key failure bug fixed

--- modules/api_requester.py
+++ modules/api_requester.py
@@ -6,31 +6,38 @@ import json
 import xmltodict
 import datetime
 
+
 class ApiRequester:
-    def __init__(self, authkey_path, authkey_type, base_url_path):
+    def __init__(self, authkey_path, authkey_type, echo=True):
         self.urllist = []
         self.type = authkey_type
-        with open(base_url_path, "r") as json_file:
+        with open("configs/api_preset.json", "r", encoding="utf-8") as json_file:
             key_list = json.load(json_file)
-            self.base_url = key_list[authkey_type]
+            self.base_url = key_list["api_key_list"][authkey_type]["base_url"]
+            self.authkey_name = key_list["api_key_list"][authkey_type]["authkey_name"]
         with open(authkey_path, "r") as json_file:
             key_list = json.load(json_file)
             self.authkey = key_list[authkey_type]
+        self.echo = echo
         self.log('authorization key is set successfully.')
 
     def log(self, content):
-        print('getAPI_{}> {}'.format(self.type, content))
+        if self.echo:
+            print('ApiRequester {}> \033[92m{}'.format(
+                self.type, content) + '\033[0m')
 
     def loadInput(self, input_path):
-        with open(input_path, "r") as json_file:
+        with open(input_path, "r", encoding="utf-8") as json_file:
             input = json.load(json_file)
-            self.setInputs(input['input_base'], input['list_key'], input['list_val'])
+            self.setInputs(input['input_base'],
+                           input['list_key'], input['list_val'])
 
-    def setInput(self, input, is_direct = True):
+    def setInput(self, input, is_direct=True):
         new_dict = {}
         for key, val in input.items():
             new_dict[parse.quote_plus(key)] = val
-        self.urllist.append('{}?authkey={}&format=xml&{}'.format(self.base_url, self.authkey, parse.urlencode(new_dict)))
+        self.urllist.append('{}?{}={}&format=xml&{}'.format(
+            self.base_url, self.authkey_name, self.authkey, parse.urlencode(new_dict)))
         if is_direct:
             self.log('input link appended successfully.')
 
@@ -45,40 +52,44 @@ class ApiRequester:
     def setOutputName(self, path, name):
         self.save_name = '{}/{}.json'.format(path, name)
         self.save_name_raw = '{}/{}_raw.json'.format(path, name)
-    
+
     def getInputs(self):
         for url in self.urllist:
             self.log(url)
-        self.log('got all input links successfully: total {} inputs.'.format(len(self.urllist)))
+        self.log('got all input links successfully: total {} inputs.'.format(
+            len(self.urllist)))
 
     def _requestOuputs(self):
         output = []
         for url in self.urllist:
             req = request.Request(url)
             req.get_method = lambda: 'GET'
-            req_dict = json.loads(json.dumps(xmltodict.parse(request.urlopen(req).read()), indent=4, ensure_ascii=False))
+            req_dict = json.loads(json.dumps(xmltodict.parse(
+                request.urlopen(req).read()), indent=4, ensure_ascii=False))
             output.append(req_dict)
         self.log('got all requested data successfully.')
         return output
 
-    def _dictExtractorByKeys(self, data, keys, is_parent = True):
+    def _dictExtractorByKeys(self, data, keys, is_parent=True):
         if is_parent == True:
             data_list = []
             for each_data in data:
-                data_list.extend(self._dictExtractorByKeys(each_data, keys, False)['vals'])
+                data_list.extend(self._dictExtractorByKeys(
+                    each_data, keys, False)['vals'])
         else:
             key_data_list = []
             val_data_list = []
             for key in keys:
                 key_val, key_path = self._findkeyPath(data, key)
-                self.log('Found path of [{}]: [{}]'.format(key, ' -> '.join(key_path)))
+                self.log('Found path of [{}]: [{}]'.format(
+                    key, ' -> '.join(key_path)))
                 key_data_list.append({'val': key_val, 'path': key_path})
             for key_data in key_data_list:
                 if key_data['val']:
-                   val_data_list.append(self._getValByPath(data, key_data['path']))
+                    val_data_list.append(
+                        self._getValByPath(data, key_data['path']))
             data_list = self._flipList(val_data_list)
         return {'keys': keys, 'vals': data_list}
-        
 
     def _flipList(self, data):
         print(data)
@@ -109,9 +120,8 @@ class ApiRequester:
                 val = data[path[0]]
             # print(val)
             return val
-        except: 
+        except:
             return -1
-
 
     def _findkeyPath(self, data, key):
         val = False
@@ -120,7 +130,7 @@ class ApiRequester:
                 # print(each_data)
                 sub_val, path = self._findkeyPath(each_data, key)
                 if sub_val:
-                    val=True
+                    val = True
                     path.insert(0, '<list>')
                     break
         else:
@@ -153,21 +163,22 @@ class ApiRequester:
             dict_list.append(temp_dict)
         return dict_list
 
-
-    def getOutputs(self, keys = -1, output_type='json'): # or csv
+    def getOutputs(self, keys=-1, output_type='json'):  # or csv
         if keys == -1:
             data_converted = self._requestOuputs()
-            with open(self.save_name_raw, "w") as json_file:
-                    json.dump(data_converted, json_file, indent = 4, ensure_ascii=False)
+            with open(self.save_name_raw, "w", encoding="utf-8") as json_file:
+                json.dump(data_converted, json_file,
+                          indent=4, ensure_ascii=False)
         else:
             output = self._requestOuputs()
             data = self._dictExtractorByKeys(output, keys)
             if output_type == 'json':
                 data_converted = self._toJson(data)
-                with open(self.save_name, "w") as json_file:
-                    json.dump(data_converted, json_file, indent = 4, ensure_ascii=False)
+                with open(self.save_name, "w", encoding="utf-8") as json_file:
+                    json.dump(data_converted, json_file,
+                              indent=4, ensure_ascii=False)
         self.log('file successfully written.')
-        
+
 
 if __name__ == '__main__':
     base_url = "http://openapi.nsdi.go.kr/nsdi/LandUseService/wfs/getLandUseWFS"
@@ -186,13 +197,13 @@ if __name__ == '__main__':
     get_api.setOutputName('.', 'tempfile2')
     get_api.getInputs()
     get_api.getOuputs()
-    sample_data = {'f': {'e':[{'a':{'c':1, 'd':2}, 'b':3}, {'a':{'c':4, 'd':5}, 'b':6}, {'a':{'c':7, 'd':8}, 'b':9}], 'f':[{'aa':{'cc':3, 'dc':4}, 'bb':2}, {'aa':{'cc':6, 'dd':7}, 'bc':5}, {'aa':{'cc':9, 'dd':10}, 'bb':8}]}}
-    print(get_api._toJson(get_api._dictExtractorByKeys(sample_data, ['a', 'b'])))
+    sample_data = {'f': {'e': [{'a': {'c': 1, 'd': 2}, 'b': 3}, {'a': {'c': 4, 'd': 5}, 'b': 6}, {'a': {'c': 7, 'd': 8}, 'b': 9}], 'f': [
+        {'aa': {'cc': 3, 'dc': 4}, 'bb': 2}, {'aa': {'cc': 6, 'dd': 7}, 'bc': 5}, {'aa': {'cc': 9, 'dd': 10}, 'bb': 8}]}}
+    print(get_api._toJson(
+        get_api._dictExtractorByKeys(sample_data, ['a', 'b'])))
     keys_request = ["NSDI:PNU",
                     "NSDI:LD_CPSG_CODE",
                     "NSDI:LD_EMD_LI_CODE",
                     "NSDI:REGSTR_SE_CODE",
                     "NSDI:MNNM"]
     get_api.getOutputs(keys_request)
-
-

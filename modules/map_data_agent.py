15444d3047da896635daeb29893137674fc270d9
ChangyongKim0 <changyong.kim.0@gmail.com>
2021-11-01 08:18:11 +0000

    slight change

--- modules/map_data_agent.py
+++ modules/map_data_agent.py
@@ -1,7 +1,5 @@
 import os
-import datetime
 import json
-import lat_lng_code_generator
 
 
 class MapDataAgent:
@@ -48,40 +46,6 @@ class MapDataAgent:
                 "{}/../data/land_data/dist/{}".format(self.current_path, lon))
             lon_lat_list[lon] = [lat.split(".")[0] for lat in lat_list]
         return lon_lat_list
-
-    # def createDBType(self, service_name):
-    #     distribution_data = self._saveLatLngCodeData(service_name)
-    #     giant_data = {}
-    #     key_list = []
-    #     file_list = os.listdir(
-    #         "{}/../data/bldg_data/raw/{}".format(os.path.dirname(__file__), service_name))
-    #     for file_name in file_list:
-    #         with open("{}/../data/bldg_data/raw/{}/{}".format(os.path.dirname(__file__), service_name, file_name), "r", encoding="utf-8") as f:
-    #             data = json.load(f)
-    #         for key in data[0].keys():
-    #             key_list.append(key)
-    #         for each_data in data:
-    #             pnu = each_data["NSDI:PNU"]
-    #             latlng = distribution_data[pnu]
-    #             lat, lng = latlng["lat_code"], latlng["lng_code"]
-    #             if pnu not in giant_data.keys():
-    #                 giant_data[pnu] = {"lat_code": lat, "lng_code": lng}
-    #             for key, val in each_data.items():
-    #                 giant_data[pnu][key] = val
-    #     key_list = set(key_list)
-    #     scheme = {}
-    #     for key in key_list:
-    #         if ("_PCLND" in key) or ("_code" in key):
-    #             scheme[key] = "int"
-    #         else:
-    #             scheme[key] = "str"
-    #     dict_list = {"scheme": scheme, "data": giant_data}
-    #     with open("{}/../output/bldg_data_db_type.json".format(os.path.dirname(__file__)), "w", encoding="utf-8") as f:
-    #         json.dump(dict_list, f, indent=4, ensure_ascii=False)
-    # ["id", "service_name", "LND_SHAPE"]
-    # ["id", transaction.list 중 id가 제일 큰거의 "NRG_DL_M", "NRF_DL_D", "NRG_AR_"]
-    # ["id", "service_name", "MAP_SHAPE", MAP]
-    # MAP_LAT MAP_LON
 
     def create(self):
         self._createDir(self.current_path+'/../data/map_data')
